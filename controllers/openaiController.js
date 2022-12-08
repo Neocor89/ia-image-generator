6213e4b26de6039b2ed777cc5965c473b528ca38
Ben <ncor89b@gmail.com>
2022-12-08 19:57:42 +0100

    backend completed

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,44 @@
+// OPEN_IA Configuration
+const { Configuration, OpenAIApi } = require("openai");
+const dotenv = require("dotenv");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// Image Generator Configuration
 const generatorImage = async (req, res) => {
-  res.status(200).json({
-    success: true,
-  });
+  const { prompt, size } = req.body;
+
+  const imageSize =
+    size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024";
+
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: imageSize,
+    });
+
+    const imageUrl = response.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+    res.status(400).json({
+      success: false,
+      error: "Image not generated",
+    });
+  }
 };
 
 module.exports = { generatorImage };
