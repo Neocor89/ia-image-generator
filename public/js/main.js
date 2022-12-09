b689c42ac21a6c01e3430f4d63644e8862fc2883
Ben <ncor89b@gmail.com>
2022-12-09 14:33:16 +0100

    Front backend connection completed start Responsive design

---
+++ public/js/main.js
@@ -0,0 +1,60 @@
+function onSubmit(e) {
+  e.preventDefault();
+
+  document.querySelector(".msg").textContent = "";
+  document.querySelector("#image").src = "";
+
+  const prompt = document.querySelector("#prompt").value;
+  const size = document.querySelector("#size").value;
+
+  if (prompt == "") {
+    alert("Please enter your image description");
+    return;
+  }
+
+  //: function generate AI Image
+  generatorImageRequest(prompt, size);
+}
+
+async function generatorImageRequest(prompt, size) {
+  try {
+    showLoader();
+
+    const response = await fetch("/openia/generateimage", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        prompt,
+        size,
+      }),
+    });
+
+    if (!response.ok) {
+      hideLoader();
+      throw new Error("Image not generated");
+    }
+
+    const data = await response.json();
+    // console.log(data);
+
+    const iaImage = data.data;
+
+    document.querySelector("#image").src = iaImage;
+
+    hideLoader();
+  } catch (error) {
+    document.querySelector(".msg").textContent = error;
+  }
+}
+
+function showLoader() {
+  document.querySelector(".loader-effect").classList.add("show");
+  document.querySelector(".loader").classList.add("show");
+}
+
+function hideLoader() {
+  document.querySelector(".loader-effect").classList.remove("show");
+  document.querySelector(".loader").classList.remove("show");
+}
+
+document.querySelector("#image-form").addEventListener("submit", onSubmit);
