5b164755985ae4c84cc2f00f41ca493f58436099
Ben <ncor89b@gmail.com>
2022-12-23 20:44:32 +0100

    added cors library

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const app = express();
 
-// Body Parser
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+//: Body Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// static Data
+//: static Data
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/openia", require("./routes/openiaRoutes"));
